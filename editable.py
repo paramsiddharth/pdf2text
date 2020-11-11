8c11bef5b167041a743e0a10f1fa2bde03b84499
harshmarolia <53485461+HarshMarolia@users.noreply.github.com>
2020-11-11 16:17:31 +0530

    Added required Imports, logs and verified the file

--- editable.py
+++ editable.py
@@ -2,35 +2,24 @@
 # The function must save the output to "./out/output.txt".
 
 # Imports
-import pyttsx3 # Speech synthesis
-import PyPDF2 # For parsing PDF documents
-from sys import stderr # For displaying errors
-from pathlib import Path # For verifying the file
+import pyttsx3  # Speech synthesis
+import PyPDF2  # For parsing PDF documents
+from sys import stderr  # For displaying errors
+from pathlib import Path  # For verifying the file
 
 # TODO
 # The extraction function
+
+
 def extract(filename):
-	book_file = input('Enter the name of the file: ')
-	# Creating output folders 
+    with open(book_file, 'a') as book:
+	pdfReader = PyPDF2.PdfFileReader(book)
+        pages = pdfReader.numPages
+	print(f'Initializing the parser object')
+		
+    with open(book_file, 'a') as book:
 	dirs = os.path.join(os.getcwd(), 'out', 'output.txt')
-	if not Path(dirs).is_dir():
-		os.makedirs(dirs)
-	if Path(f'./{book_file}').is_file():
-	    # Using the with-as syntax eliminates the need to close the file object manually and is safer
-	    with open(book_file, 'a') as book:
-		# Initialize the PDF file object
-		pdfReader = PyPDF2.PdfFileReader(book)
-		pages = pdfReader.numPages
-
-		# Set page_number to the first page in the case of invalid input
-		if page_number > pages:
-		    print(f'Error: {page_number} exceeds the number of available pages.', file=stderr)
-		    page_number = 1
-
-		for num in range(page_number - 1, pages):
-		    # Extract and pronounce the text for each page
-		    text = pdfReader.getPage(num).extractText()
-		    print(text)
-
-	else:
-	    print('Error: File inaccessible!', file=stderr) # Couldn't read the file
+	print(f'Writing the content to a new file')
+	for num in range(page_number - 1, pages):
+        text = pdfReader.getPage(num).extractText()
+        print(text, file=book)          
