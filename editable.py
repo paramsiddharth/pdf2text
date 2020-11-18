b029afacda0169f41fe5bb1743b663b8287a5615
harshmarolia <53485461+HarshMarolia@users.noreply.github.com>
2020-11-18 22:07:40 +0530

    Corrected the indentation error.

--- editable.py
+++ editable.py
@@ -7,19 +7,16 @@ import PyPDF2  # For parsing PDF documents
 from sys import stderr  # For displaying errors
 from pathlib import Path  # For verifying the file
 
-# TODO
-# The extraction function
-
 
 def extract(filename):
     with open(book_file, 'a') as book:
-	pdfReader = PyPDF2.PdfFileReader(book)
+        pdfReader = PyPDF2.PdfFileReader(book)
         pages = pdfReader.numPages
-	print(f'Initializing the parser object')
-		
+    print(f'Initializing the parser object')
+
     with open(book_file, 'a') as book:
-	dirs = os.path.join(os.getcwd(), 'out', 'output.txt')
-	print(f'Writing the content to a new file')
-	for num in range(page_number - 1, pages):
-        text = pdfReader.getPage(num).extractText()
-        print(text, file=book)          
+        dirs = os.path.join(os.getcwd(), 'out', 'output.txt')
+        print(f'Writing the content to a new file')
+        for num in range(page_number - 1, pages):
+            text = pdfReader.getPage(num).extractText()
+            print(text, file=book)
