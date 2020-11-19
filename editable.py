80cedaa48d8a9b2dd0ef1ba63c082183bd550f67
harshmarolia <53485461+HarshMarolia@users.noreply.github.com>
2020-11-20 01:22:44 +0530

    Fixed minor issues.
    Added required library.
    Checked for the variables that are not defined.

--- editable.py
+++ editable.py
@@ -6,17 +6,17 @@ import pyttsx3  # Speech synthesis
 import PyPDF2  # For parsing PDF documents
 from sys import stderr  # For displaying errors
 from pathlib import Path  # For verifying the file
-
+import os # For directory operations
 
 def extract(filename):
-    with open(book_file, 'a') as book:
+    with open(filename, 'a') as book:
         pdfReader = PyPDF2.PdfFileReader(book)
         pages = pdfReader.numPages
     print(f'Initializing the parser object')
 
-    with open(book_file, 'a') as book:
+    with open(filename, 'a') as book:
         dirs = os.path.join(os.getcwd(), 'out', 'output.txt')
         print(f'Writing the content to a new file')
-        for num in range(page_number - 1, pages):
+        for num in range(pages):
             text = pdfReader.getPage(num).extractText()
             print(text, file=book)
